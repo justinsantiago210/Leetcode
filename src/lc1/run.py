48f51018e42a137449277ae2b433da9dfe96a23f
justinsantiago <49800180+justinsantiago210@users.noreply.github.com>
2023-09-07 17:57:47 -0400

    Brainstorming for TwoSum LC

--- src/lc1/run.py
+++ src/lc1/run.py
@@ -6,15 +6,35 @@
 
 numList = [1,2,3,4,5,6,7]
 target = 9
+i = 0
+x = 0
+
+
+#
+# print(numList[5])
+#
+# numList = numList[3] + numList[4]
+# print(numList)
+#
+# if numList == target:
+#     print('you did it, it is 9!')
+# else:
+#     print('try again')
+#
+
+## iterate through numList,
+# if both numbers add to target, save to output and return the value of the iterators (which will be the indicies)
 
 
 
-print(numList[5])
-
-numList = numList[3] + numList[4]
-print(numList)
-
-if numList == target:
-    print('you did it, it is 9!')
-else:
-    print('try again')
+#------TESTING ENUMERATION----
+#https://www.geeksforgeeks.org/enumerate-in-python/#
+# l1 = [10,12,'hi',7,0.5]
+# obj1 = enumerate(l1)
+#
+# print('Return Type ', type(obj1))
+# print(list(obj1))
+#
+#
+# for count, ele in enumerate(l1) :
+#     print(count, ele)
